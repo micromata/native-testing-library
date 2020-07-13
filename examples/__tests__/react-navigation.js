96b6b3f90e0bbd2e83791780170a7f701ae7673f
daveols <10344370+daveols@users.noreply.github.com>
2020-07-13 18:18:04 +1000

    update react-navigation test example to use v4

--- examples/__tests__/react-navigation.js
+++ examples/__tests__/react-navigation.js
@@ -1,7 +1,8 @@
 import '@testing-library/jest-native/extend-expect';
 import React from 'react';
 import { Button, Text, View } from 'react-native';
-import { createStackNavigator, createAppContainer, withNavigation } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer, withNavigation } from 'react-navigation';
 
 import { render, fireEvent } from '../../src';
 
